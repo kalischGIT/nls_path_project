508ab8f92cf3310707e215dfa174431d3f65b8b8
cwcurtis <ccurtis@mail.sdsu.edu>
2018-01-04 12:05:27 -0800

    More Updates

--- Matlab_Code_and_Figures/zero_return.m
+++ Matlab_Code_and_Figures/zero_return.m
@@ -2,17 +2,18 @@ function zero_return(Pvals,sig,tol)
 
 kvals = linspace(0,20,Pvals);
 omvals = zeros(length(kvals),1);
-omvals_wrong = zeros(length(kvals),1);
+%omvals_wrong = zeros(length(kvals),1);
 
 for jj=1:length(kvals)
     omvals(jj) = zero_lan(kvals(jj),sig,tol);
-    omvals_wrong(jj) = zero_lan_wrong(kvals(jj),sig,tol);
+    %omvals_wrong(jj) = zero_lan_wrong(kvals(jj),sig,tol);
 end
 
 figure(1)
-plot(kvals,omvals,'k-',kvals,omvals_wrong,'k--','LineWidth',2)
+%plot(kvals,omvals,'k-',kvals,omvals_wrong,'k--','LineWidth',2)
+plot(kvals,omvals,'k-','LineWidth',2)
 h = set(gca,'FontSize',30);
 set(h,'Interpreter','LaTeX')
 xlabel('$k_{0}$','Interpreter','LaTeX','FontSize',30)
 ylabel('$\omega$','Interpreter','LaTeX','FontSize',30)
-legend({'$u^{L}_{p}(k_{0},\omega)$','$u^{L,a}_{p}(k_{0},\omega)$'},'Interpreter','LaTeX')
+%legend({'$u^{L}_{p}(k_{0},\omega)$','$u^{L,a}_{p}(k_{0},\omega)$'},'Interpreter','LaTeX')
